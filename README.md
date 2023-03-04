75f364286f1fb0c3e0ba26cbf8f950be38e06915
Abdurrahman Pektas <apektas@yandex.com>
2023-03-04 14:10:30 +0300

    Backend is dockerized! M1 users should force docker to use linux/amd64 platform via the following command
    export DOCKER_DEFAULT_PLATFORM=linux/amd64

---
+++ README.md
@@ -0,0 +1,9 @@
+# Ounass Facebook Marketing Assignment
+
+**_APPLE M1 USERS:_**  set the environment variable DOCKER_DEFAULT_PLATFORM as follows otherwise you will run into the database connection issue. 
+export DOCKER_DEFAULT_PLATFORM=linux/amd64 
+
+
+## How to run
+
+docker compose build --no-cache
--- backend/app/database.py
+++ backend/app/database.py
@@ -3,10 +3,11 @@ import logging
 import sqlalchemy as _sql
 import sqlalchemy.ext.declarative as _declarative
 import sqlalchemy.orm as _orm
+import os
 from app.config import DB_USER, DB_HOST, DB_NAME, DB_PASSWORD
 
 # DATABASE_URL = f"postgresql://postgres:ounass@localhost/ounass"
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
 
 logging.info(f"Database connection URL - {DATABASE_URL}")
 
